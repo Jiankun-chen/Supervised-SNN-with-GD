c9317e1d0059c5fdf682344d06060866a535f82f
Jiankun-chen <79264897+Jiankun-chen@users.noreply.github.com>
2022-01-15 11:08:46 +0000

    Delete SUPSNN directory

--- SUPSNN/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from .placeholder import placeholder_inputs
-from .GetData import GetData
-from .evaluate import *
-from .training import training
-from .vis import vis_synapse
-from .inference import *
-from .recep_field import *
-
